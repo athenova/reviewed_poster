8004c1b5db274aeba95075a1828fa1de7009482f
athenova <aleksey.sergeyev@yandex.com>
2025-04-11 08:19:59 +0300

    Поправлены имена модулей

--- projects/one_day_word.py
+++ projects/one_day_word.py
@@ -1,5 +1,5 @@
 from simple_blogger.blogger.basic import SimpleBlogger
-from simple_blogger.poster.TelegramPoster import TelegramPoster
+from simple_blogger.poster.telegram import TelegramPoster
 from simple_blogger.editor import Editor
 from simple_blogger.preprocessor.text import TagAdder
 from simple_blogger.generator.openai import OpenAiTextGenerator
