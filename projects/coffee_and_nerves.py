3bd8143de358cd095a843bf025618da7ae1d2b27
athenova <aleksey.sergeyev@yandex.com>
2025-04-11 08:19:15 +0300

    Поправлены имена модулей

--- projects/coffee_and_nerves.py
+++ projects/coffee_and_nerves.py
@@ -1,6 +1,6 @@
 from simple_blogger.blogger.auto import AutoBlogger
-from simple_blogger.poster.TelegramPoster import TelegramPoster
-from simple_blogger.poster.VkPoster import VkPoster
+from simple_blogger.poster.telegram import TelegramPoster
+from simple_blogger.poster.vk import VkPoster
 from simple_blogger.editor import Editor
 from simple_blogger.preprocessor.text import TagAdder
 from simple_blogger.generator.openai import OpenAiTextGenerator, OpenAiImageGenerator
